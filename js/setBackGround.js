e9aac31c3bf18b1491908e555367396ea9155a6e
asvow <asvow@users.noreply.github.com>
2021-12-13 22:45:01 +0800

    Update setBackGround.js

--- js/setBackGround.js
+++ js/setBackGround.js
@@ -1,7 +1,7 @@
 window.onload=function(){
     const panelCover = document.getElementById("panel-cover");
     const rndNum = Math.floor(Math.random()*8);
-    const url = "/bing?format=js&idx=" + rndNum +"&n=1";
+    const url = "/img?format=js&idx=" + rndNum +"&n=1&setmkt=zh-cn&setlang=zh-cn";
     $.ajax({
         url: url,
         dataType: "json",
